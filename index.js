300c5bd6960aed6ae0fa8f4be35bbc785909fa3e
sanakdam <san.akdam@gmail.com>
2016-10-25 10:23:18 +0700

    Add command to show all current user's products
    Update add command to show all current user's products

--- index.js
+++ index.js
@@ -171,9 +171,31 @@ function start(token, username){
     });
   });
 
+  /**
+   * Me Products
+   * See all Products created by current user
+   */
+  vorpal
+  .command('me products', 'See all products created by current user')
+  .action(function(args, callback) {
+    me(token, function(response){
+      var makerOf = response.body.user.makerOf;
+
+      makerOf.forEach(function(product) {
+        vorpal.log('\n  ' + chalk.bold.blue(product.name));
+        vorpal.log('  ' + chalk.italic(product.tagline));
+        vorpal.log('  ' + chalk.dim('Votes') + ': ' +
+                  chalk.bold(product.votes_count) + '   ' +
+                  chalk.dim('Comments') + ': ' +
+                  chalk.bold(product.comments_count));
+        vorpal.log('  ' + chalk.italic.underline.dim(product.discussion_url) + '\n');
+      })
+      callback();
+    });
+  });
+
   vorpal.delimiter('@' + username + ' => ').show();
 }
-
 
 // Here we go!
 init();
